d20557141820ab2a50f4ed6d682c37916bebb479
amaresh252 <102523445+amaresh252@users.noreply.github.com>
2023-07-11 22:07:52 +0530

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,47 @@
+const socket = io('http://localhost:700');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+
+
+
+ const append =(message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement); 
+    scrollToBottom()
+ }
+ 
+ form.addEventListener('submit', (e)=> {
+   e.preventDefault();
+   const message  = messageInput.value;
+   append(`You: ${message}`, 'right');
+   socket.emit('send', message);
+   messageInput.value = ''
+})
+
+
+ const name = prompt("Enter your name to join");
+ socket.emit('new-user-joined',name);
+
+ socket.on('user-joined', name =>{
+    append(`${name} joined the chat`, 'right')
+ })
+
+ socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left')
+    scrollToBottom()
+ })
+
+ socket.on('left', name =>{
+    append(`${name} left the chat`, 'right')
+ })
+
+ 
+
+function scrollToBottom() {
+   messageContainer.scrollTop = messageContainer.scrollHeight
+}
